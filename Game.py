5ae7159f18d718533feb63f110cc400823660b78
aliev <sambufer200@gmail.com>
2019-10-22 21:59:03 +0300

    add handler key function

--- Game.py
+++ Game.py
@@ -1,15 +1,14 @@
 import pygame
-import sys
-
-pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 WINDOW_TITLE = "SPACE-X LENDER"
 GAME_FPS = 60
 SIZE = (WIDTH, HEIGHT)
 SCORE = 100
-green = 120, 240, 120
-white = 255, 255, 255
+GREEN = (120, 240, 120)
+WHITE = (255, 255, 255)
+
+pygame.init()
 
 speed = [1, 1]
 
@@ -17,23 +16,32 @@ CLOCK = pygame.time.Clock()
 
 screen = pygame.display.set_mode(SIZE)
 
-font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Emin rulit', True, white, None)
-textRect = text.get_rect()
-textRect.center = (WIDTH//2, HEIGHT//2)
+"""def gamePlay():
+    rocket = pygame.Rect(WIDTH // 2), HEIGHT - 180, 60, 180)"""
 
-while 1:
-    for event in pygame.event.get():
+
+def control(self, events):
+    """
+    Функция, отвещающая за обработку нажатия клавиш.
+    """
+    for event in events:
         if event.type == pygame.QUIT:
-            sys.exit()
-
-    textRect = textRect.move(speed)
-    textRect = textRect
-    if textRect.left < 0 or textRect.right > WIDTH:
-        speed[0] = -speed[0]
-    if textRect.top < 0 or textRect.bottom > HEIGHT:
-        speed[1] = -speed[1]
-
-    screen.fill(green)
-    screen.blit(text, textRect)
-    pygame.display.flip()
+            return 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                self.y_speed = 1
+            if event.key == pygame.K_UP:
+                self.y_speed = 1
+            if event.key == pygame.K_RIGHT:
+                self.x_speed = 1
+            if event.key == pygame.K_LEFT:
+                self.x_speed = 1
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.x_speed = -2
+            if event.key == pygame.K_RIGHT:
+                self.x_speed = 2
+            if event.key == pygame.K_UP:
+                self.y_speed = -2
+            if event.key == pygame.K_DOWN:
+                self.y_speed = 2
