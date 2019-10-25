7483a6fa03d97df9570f6f39b5bac8335ea9f4fe
Alexander Ermakov <alexermakovx@gmail.com>
2019-10-25 21:08:57 +0300

    added test mechanic

--- Game.py
+++ Game.py
@@ -1,47 +1,63 @@
 import pygame
+from Rocket import Rocket
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1280, 720
 WINDOW_TITLE = "SPACE-X LENDER"
 GAME_FPS = 60
 SIZE = (WIDTH, HEIGHT)
 SCORE = 100
 GREEN = (120, 240, 120)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+rocket = Rocket(WIDTH/2, HEIGHT/2, GREEN, [0, 1])
 
 pygame.init()
-
-speed = [1, 1]
-
+pygame.display.set_caption(WINDOW_TITLE)
 CLOCK = pygame.time.Clock()
-
 screen = pygame.display.set_mode(SIZE)
+run = True
 
-"""def gamePlay():
-    rocket = pygame.Rect(WIDTH // 2), HEIGHT - 180, 60, 180)"""
+while run:
 
+    CLOCK.tick(GAME_FPS)
 
-def control(self, events):
-    """
-    Функция, отвещающая за обработку нажатия клавиш.
-    """
-    for event in events:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return 1
+            run = False
+
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_UP]:
+        #     rocket.y_speed_plus(-5)
+
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                self.y_speed = 1
+            # if event.key == pygame.K_DOWN:
+            #     pass
             if event.key == pygame.K_UP:
-                self.y_speed = 1
+                rocket.y_speed_plus(3)
             if event.key == pygame.K_RIGHT:
-                self.x_speed = 1
+                pass
             if event.key == pygame.K_LEFT:
-                self.x_speed = 1
+                pass
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                self.x_speed = -2
+                rocket.x_speed_plus(-1)
             if event.key == pygame.K_RIGHT:
-                self.x_speed = 2
+                rocket.x_speed_plus(1)
             if event.key == pygame.K_UP:
-                self.y_speed = -2
-            if event.key == pygame.K_DOWN:
-                self.y_speed = 2
+                rocket.y_speed_plus(-3)
+            # if event.key == pygame.K_DOWN:
+            #     pass
+
+    rocket.move(screen)
+    screen.fill(BLACK)
+    rocket.draw(screen)
+    pygame.display.update()
+
+pygame.quit()
+
+
+
+
+
--- Rocket.py
+++ Rocket.py
@@ -1,12 +1,36 @@
 import time
+import pygame
 
 
 class Rocket:
     def __init__(self, x, y, color, speed):
         self.x = x
         self.y = y
+        self.width = 50
+        self.height = 100
         self.color = color
         self.acceleration = False  # ускорение
         self.startAcceleration = time.time()
         self.fuel = 1000
         self.speed = speed
+
+    def move_up(self):
+        self.y = self.y + 2
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, screen):
+        width, height = screen.get_size()
+        if (self.x < width - self.width) and (self.x > 0) and (self.y > 0) and (self.y < height - self.height):
+            print(self.speed[1])
+            self.y += self.speed[1] + 1
+            self.x += self.speed[0]
+        elif self.y < height - self.height:
+            self.speed = [0, 1]
+
+    def x_speed_plus(self, speed):
+        self.speed[0] += speed
+
+    def y_speed_plus(self, speed):
+        self.speed[1] += speed
