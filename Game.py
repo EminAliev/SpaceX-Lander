e375855c342d8c01b7cefb8b97339482ea9c9ab0
Alexander Ermakov <alexermakovx@gmail.com>
2019-10-29 20:46:56 +0300

    created normal mechanic

--- Game.py
+++ Game.py
@@ -1,15 +1,13 @@
 import pygame
 from Rocket import Rocket
+from Config import *
 
 WIDTH, HEIGHT = 1280, 720
 WINDOW_TITLE = "SPACE-X LENDER"
 GAME_FPS = 60
 SIZE = (WIDTH, HEIGHT)
 SCORE = 100
-GREEN = (120, 240, 120)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-SPEED = [0, 1]
+SPEED = 0.05
 
 pygame.init()
 pygame.display.set_caption(WINDOW_TITLE)
@@ -17,9 +15,28 @@ CLOCK = pygame.time.Clock()
 screen = pygame.display.set_mode(SIZE)
 run = True
 
+
 all_sprites = pygame.sprite.Group()
-rocket = Rocket(WIDTH / 2, HEIGHT / 2, GREEN, SPEED)
+# rocket = Rocket(WIDTH / 2, HEIGHT / 2, GREEN, SPEED)
+rocket = Rocket(SPEED, (0, 0.01), (WIDTH / 2, HEIGHT / 2))
 all_sprites.add(rocket)
+platform = pygame.Surface((30, 1))
+pygame.draw.line(platform, WHITE, (0, 0), (30, 0), 1)
+playersprite = pygame.sprite.RenderPlain(rocket)
+# platform.fill(WHITE)
+
+
+def draw():
+    playersprite.update()
+    # rocket.move(screen)
+    rocket.update()
+    screen.fill(BLACK)
+    playersprite.draw(screen)
+    # screen.blit(rocket.image, (rocket.x, rocket.y))
+    screen.blit(platform, (WIDTH / 2 - 15, HEIGHT - 5))
+    # all_sprites.draw(screen)
+    pygame.display.update()
+
 
 while run:
 
@@ -35,27 +52,25 @@ while run:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                rocket.x_speed_plus(-1)
+                # rocket.x_speed_plus(-1)
+                rocket.angle_speed = -ANGLE
             if event.key == pygame.K_RIGHT:
-                rocket.x_speed_plus(1)
+                # rocket.x_speed_plus(1)
+                rocket.angle_speed = +ANGLE
             if event.key == pygame.K_UP:
-                rocket.y_speed_plus(-3)
+                rocket.gas = True
 
         if event.type == pygame.KEYUP:
             # if event.key == pygame.K_DOWN:
             #     pass
             if event.key == pygame.K_UP:
-                rocket.y_speed_plus(3)
+                rocket.gas = False
             if event.key == pygame.K_RIGHT:
-                pass
+                rocket.angle_speed = 0
             if event.key == pygame.K_LEFT:
-                pass
+                rocket.angle_speed = 0
             # if event.key == pygame.K_DOWN:
             #     pass
-    rocket.move(screen)
-    screen.fill(BLACK)
-    screen.blit(rocket.image, (rocket.x, rocket.y))
-    # all_sprites.draw(screen)
-    pygame.display.update()
+    draw()
 
 pygame.quit()
--- Rocket.py
+++ Rocket.py
@@ -1,51 +1,51 @@
 import time
 import pygame
+from pygame.math import Vector2
 
 
 class Rocket(pygame.sprite.Sprite):
-    """def __init__(self, x, y, color, speed):
-        self.x = x
-        self.y = y
-        self.width = 50
-        self.height = 100
-        self.color = color
-        self.acceleration = False  # ускорение
-        self.startAcceleration = time.time()
-        self.fuel = 1000
-        self.speed = speed"""
 
-    def __init__(self, x, y, color, speed):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("spaceship1-orange.png").convert_alpha()
+    def __init__(self, speed, gravity_val=(0, 0.01), pos=(0, 0)):
+        super(Rocket, self).__init__()
+        size = (24, 28)  # Create "wrapper" for image
+        self.image = pygame.Surface(size)
+        sprite = pygame.image.load("test-sprite-24x28.png").convert_alpha()  # load sprite
+        self.image.blit(sprite, (0, 0))
+        self.original_image = self.image
         self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = [0, 200]
-        self.x = x
-        self.y = y
-        self.width = 50
-        self.height = 100
-        self.acceleration = False  # ускорение
-        self.startAcceleration = time.time()
-        self.fuel = 1000
+        self.position = Vector2(pos)
+        self.move_direction = Vector2(0, 0)
+        self.shift_direction = Vector2(0, -1)
         self.speed = speed
-        self.color = color
-
-    def move_up(self):
-        self.y = self.y + 2
-
-    """def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))"""
-
-    def move(self, screen):
-        width, height = screen.get_size()
-        if (self.x < width - self.width) and (self.x > 0) and (self.y > 0) and (self.y < height - self.height):
-            print(self.speed[1])
-            self.y += self.speed[1] + 1
-            self.x += self.speed[0]
-        elif self.y < height - self.height:
-            self.speed = [0, 1]
+        self.angle = 0
+        self.angle_speed = 0
+        self.x, self.y = pos
+        self.width, self.height = size
+        self.gas = False
+        self.gravity_val = gravity_val
 
     def x_speed_plus(self, speed):
         self.speed[0] += speed
 
     def y_speed_plus(self, speed):
         self.speed[1] += speed
+
+    def gravity(self):
+        self.move_direction += Vector2(self.gravity_val)
+
+    def update(self):
+        if self.angle_speed != 0:
+            # Rotate the direction vector and then the image.
+            self.shift_direction.rotate_ip(self.angle_speed)
+            self.angle += self.angle_speed
+            self.image = pygame.transform.rotate(self.original_image, -self.angle)
+            self.rect = self.image.get_rect(center=self.rect.center)
+            # Update the position vector and the rect.
+        if self.gas:
+            self.move_direction += self.shift_direction * self.speed
+        self.gravity()
+        self.position += self.move_direction * 1
+        self.rect.center = self.position
+
+    def shift(self, speed):
+        self.move_direction += self.shift_direction * self.speed
